2b484a4c9deadf23af9eee3e76c798b1f082e2bc
IEnumerablee <thepashalza@gmail.com>
2025-03-21 19:15:28 +0500

    Inital commit

---
+++ src/main/java/org/ienumerable/wonderland/Wonderland.java
@@ -0,0 +1,17 @@
+package org.ienumerable.wonderland;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Wonderland extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
